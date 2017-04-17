3ea0cee75c05eaf723a3c2d3fb4148696451cc1c
Robert Sevumyan <alroberto@rambler.ru>
2017-04-17 22:25:42 +0300

    First commit

---
+++ init.el
@@ -0,0 +1,44 @@
+(package-initialize)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
+(load "package")
+
+(require 'package)
+
+(setq package-archives '(
+			 ("elpy" . "http://jorgenschaefer.github.io/packages/")
+			 ("gnu" . "http://elpa.gnu.org/packages/")
+			 ("marmalade" . "http://marmalade-repo.org/packages/")
+			 ("melpa" . "http://melpa.milkbox.net/packages/")                         
+			 ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
+			 ("org" . "http://orgmode.org/elpa/")
+			 ))
+
+
+(require 'ido)
+
+(require 'multi-term)
+
+(setq multi-term-program "/bin/bash")
+
+(ido-mode t)
+
+(add-hook 'json-mode-hook #'flycheck-mode)
+
+(custom-set-variables
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(column-number-mode t)
+ '(custom-enabled-themes nil)
+ '(package-selected-packages (quote (json-mode)))
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil))
+
+(custom-set-faces)
+
+(put 'upcase-region 'disabled nil)
