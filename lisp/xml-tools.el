1dcb1a95d22cc64e35d787eeec067ee937aed350
dovatron <alroberto@rambler.ru>
2017-04-19 22:51:32 +0300

    Перенес функции работы с xml в отдельный файл. Добавил цветовую схему.

---
+++ lisp/xml-tools.el
@@ -0,0 +1,25 @@
+(provide 'xml-tools)
+
+(add-to-list 'auto-mode-alist
+	     (cons (concat "\\." (regexp-opt '("xml" "xsd" "sch" "rng" "xslt" "svg" "rss") t) "\\'")
+		   'nxml-mode))
+
+(setq auto-mode-alist (cons '("\\.xml$" . nxml-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.xsl$" . nxml-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.xhtml$" . nxml-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.page$" . nxml-mode) auto-mode-alist))
+
+(autoload 'xml-mode "nxml" "XML editing mode" t)
+
+(eval-after-load 'rng-loc
+  '(add-to-list 'rng-schema-locating-files "~/.schema/schemas.xml"))
+
+(global-set-key [C-return] 'completion-at-point)
+
+(defun remove-newlines-in-region ()
+  "Removes all newlines in the region."
+  (interactive)
+  (save-restriction
+    (narrow-to-region (point) (mark))
+    (goto-char (point-min))
+    (while (or (search-forward "\n" nil t)) (replace-match "" nil t))))
