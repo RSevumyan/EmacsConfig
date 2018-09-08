17f7307e131a7c9140b4e5c1facf46397f622af9
dovatron <alroberto@rambler.ru>
2018-09-08 17:00:40 +0300

    some updates

--- init.el
+++ init.el
@@ -2,32 +2,33 @@
 
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+
 (require 'package)
 
 (setq package-archives '(
 			 ("elpy" . "http://jorgenschaefer.github.io/packages/")
 			 ("gnu" . "http://elpa.gnu.org/packages/")
 			 ("melpa" . "http://melpa.milkbox.net/packages/")                         
-			 ("melpa-stable" . "https://stable.melpa.org/packages/")
+			 ("melpa-stable" . "https://stableи.melpa.org/packages/")
 			 ("org" . "http://orgmode.org/elpa/")
 			 ))
 
 (package-initialize)
 
 (require 'ido)
-
+ 
 (ido-mode t)
 
 (require 'multi-term)
+
+(require 'common)
 
 (setq multi-term-program "/bin/bash")
 
 (require 'xml-tools)
 
 (require 'sr-speedbar)
-
-(sr-speedbar-open)
-
 
 (add-hook 'json-mode-hook #'flycheck-mode)
 
@@ -44,18 +45,23 @@
 
 (global-linum-mode)
 
+(global-key (kbd "C-c C-r") 'revert-buffer)
 (global-set-key (kbd "C-c C-r") 'revert-buffer)
+(global-set-key (kbd "M-<down>") 'forward-paragraph)
+(global-set-key (kbd "M-<up>") 'backward-paragraph)
+(global-set-key (kbd "C-<down>") 'goto-tenlines-forward)
+(global-set-key (kbd "C-<up>") 'goto-tenlines-backward)
+(global-set-key (kbd "M-<left>") 'goto-twentycolumns-backward)
+(global-set-key (kbd "M-<right>") 'goto-twentycolumns-forward)
+(global-set-key (kbd "M-a") 'beginning-of-buffer)
+(global-set-key (kbd "M-e") 'end-of-buffer)
 
 (set-background-color "#fffaf0")
-
-(defun toggle-fullscreen ()
-  (interactive)
-  (when (eq window-system 'x)
-    (set-frame-parameter
-     nil 'fullscreen
-     (when (not (frame-parameter nil 'fullscreen)) 'fullboth))))
+(set-face-attribute 'region nil :background "#FFB72F")
 
 (toggle-fullscreen)
+
+(tool-bar-mode -1)
 
 ;;==============================================================================================================================================================
 ;;=============================================================Package initialization===========================================================================
@@ -69,8 +75,12 @@
   (package-refresh-contents)
   (package-install 'image+))
 
+(unless (package-installed-p 'vlf)
+  (package-refresh-contents)
+  (package-install 'vlf))
+
 ;;==============================================================================================================================================================
-;;====================================================================Custom set================================================================================
+;;====================================================================Custom settings===========================================================================
 ;;==============================================================================================================================================================
 
 
@@ -85,17 +95,13 @@
    ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
  '(column-number-mode t)
  '(custom-enabled-themes nil)
- '(package-selected-packages (quote (image+ magit json-mode)))
- '(speedbar-directory-unshown-regexp "\"^\\\\(\\\\..*\\\\)\\\\'\"")
- '(speedbar-show-unknown-files t)
-l '(sr-speedbar-default-width 20)
- '(sr-speedbar-max-width 20)
+ '(package-selected-packages
+   (quote
+    (markdown-preview-mode markdown-mode vlf image+ magit json-mode)))
  '(tool-bar-mode nil)
  '(tooltip-mode nil))
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;Dired mode cusomization
+(put 'dired-find-alternate-file 'disabled nil)
+(eval-after-load 'dired '(define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file))
+(eval-after-load 'dired '(define-key dired-mode-map (kbd "a") 'dired-find-file))
---
+++ lisp/common.el
@@ -0,0 +1,41 @@
+(provide 'common)
+
+;;Function for emacs frame maximization
+(defun toggle-fullscreen ()
+  (interactive)
+  (when (eq window-system 'x)
+    (set-frame-parameter
+     nil 'fullscreen
+     (when (not (frame-parameter nil 'fullscreen)) 'fullboth))))
+
+;;Function for move 10 lines forward
+(defun goto-tenlines-forward ()
+  (interactive)
+  (goto-line (+ (count-lines 1 (point)) 11))
+  )
+
+;;Function for move 10 lines backwardy
+(defun goto-tenlines-backward ()
+  (interactive)
+  (goto-line (- (count-lines 1 (point)) 11))
+  )
+
+;;Function for moveing 20 columns forward
+(defun goto-twentycolumns-forward ()
+  (interactive)
+  ;;Not implemented yet
+  )
+
+
+;;Function for moveing 20 columns backward
+(defun goto-twentycolumns-backward ()
+  (interactive)
+  ;;Not implemented yet
+  )
+
+(defun toggle-fullscreen ()
+  (interactive)
+  (when (eq window-system 'x)
+    (set-frame-parameter
+     nil 'fullscreen
+     (when (not (frame-parameter nil 'fullscreen)) 'fullboth))))
